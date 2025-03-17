17e478e1c56046421277c79103e33d038f3a1415
Jacob Brazeal <jacob.brazeal@gmail.com>
2025-03-17 13:24:21 -0700

    switch to malloc

--- binsearch.c
+++ binsearch.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int binsearch(int x, int v[], int n);
 // int binsearch2(int x, int v[], int n);
 
 int main()
 {
-    int n = 1000000;
-    int v[n];
+    int n = 10000000;
+    int *v = malloc(sizeof(int) * n);
     long total = 0; // to avoid gcc throwing the result away
+
+    printf("setting up array\n");
 
     for ( int i = 0; i < n; ++i ) v[i] = i + 1;
 
-
+    printf("testing binary search...\n");
     for ( int i = 0; i < n; ++i ) {
         // printf("Position of %d in array is %d\n", v[i], binsearch(v[i], v, n));
         total += binsearch(v[i], v, n);
     }
 
-    long expected = (long)n * (n - 1) / 2;
-
-    printf("OK: %d, GOT: %ld, EXPECTED: %ld\n", 
-        total == expected,
-        total,
-        expected    
+    printf("Total iters: %ld\n", 
+        total    
     );
 
 }
@@ -33,15 +32,17 @@ int binsearch(int x, int v[], int n)
     int low, high, mid;
     low = 0;
     high = n - 1;
+    int iters = 0;
     while (low <= high)
     {
+        iters ++;
         mid = (low + high) / 2;
         if (x < v[mid])
             high = mid;
         else if (x > v[mid])
             low = mid + 1;
         else /* found match */
-            return mid;
+            return iters;
     }
-    return -1; /* no match */
+    return iters; /* no match */
 }
