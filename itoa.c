e74c2e722d9b84da568071ea9f5b15ef578e7046
Jacob Brazeal <jacob.brazeal@gmail.com>
2025-03-22 21:52:07 -0700

    Exercise 3.4: Print least integer

--- itoa.c
+++ itoa.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void reverse(char s[]) {
+void reverse(char s[])
+{
     int i = 0, j = 0;
     char tmp;
-    while(s[j] != '\0') ++j;
-    for ( --j; i < j; i++, j--) {
+    while (s[j] != '\0')
+        ++j;
+    for (--j; i < j; i++, j--)
+    {
         tmp = s[i];
         s[i] = s[j];
         s[j] = tmp;
@@ -14,14 +17,16 @@ void reverse(char s[]) {
 
 void itoa(int n, char s[])
 {
-    int i, sign;
+    int i, sign, least;
     if ((sign = n) < 0) /* record sign */
         n = -n;         /* make n positive */
     i = 0;
+    least = n - 1 > n ? 1 : 0;
+
     do
-    {                          /* generate digits in reverse order */
-        s[i++] = n % 10 + '0'; /* get next digit */
-    } while ((n /= 10) > 0); /* delete it */
+    {                                                      /* generate digits in reverse order */
+        s[i++] = (least ? (~(n % 10)) + 1 : n % 10) + '0'; /* get next digit */
+    } while ((n /= 10) != 0); /* delete it */
     if (sign < 0)
         s[i++] = '-';
     s[i] = '\0';
@@ -31,10 +36,9 @@ void itoa(int n, char s[])
 int main()
 {
     char s[20];
-    for ( int i = -__INT_MAX__ - 1; i < -__INT_MAX__ + 10; i++) {
+    for (int i = -__INT_MAX__ - 1; i < -__INT_MAX__ + 10; i++)
+    {
         itoa(i, s);
         printf("%d %s\n", i, s);
     }
-
 }
-
